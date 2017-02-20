17cc227c67efa442b0b405ad4a93deb037546b3f
Aspros <Aspros_L@outlook.com>
2017-02-20 15:05:03 +0800

    提交Repository DI注入

--- Aspros_DDD_BootStrapping/Startup.cs
+++ Aspros_DDD_BootStrapping/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Aspros_DDD_Infrastructure;
+﻿using Aspros_DDD_Domain_Repository_Interfaces.UserRepositoryInterfaces;
+using Aspros_DDD_Infrastructure;
+using Aspros_DDD_Repository.UserRepositories;
 using Framework.Infrastructure.Interfaces.Core;
 using Microsoft.Extensions.DependencyInjection;
 using MySQL.Data.EntityFrameworkCore.Extensions;
@@ -9,9 +11,13 @@ namespace Aspros_DDD_BootStrapping
     {
         public static void Configure(this IServiceCollection service, string connectionString)
         {
-            service.AddDbContext<CommodityDbContext>(optionsBuilder => optionsBuilder.UseMySQL(connectionString));
+            service.AddDbContext<CommodityDbContext>(options =>options.UseMySQL(connectionString));
 
             service.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            service.AddScoped<IDbContext, CommodityDbContext>();
+
+            service.AddTransient<IUserRepository, UserRepository>();
         }
     }
 }
